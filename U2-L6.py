6825392727f33ea8c60b51688893a6d454716c39
souro268 <63824018+souro268@users.noreply.github.com>
2024-10-02 09:31:16 -0400

    Update U2-L6.py

--- U2-L6.py
+++ U2-L6.py
@@ -24,3 +24,30 @@ for j in range(count, -1, -1):
 
 
 
+
+# count = int(input("Enter number: "))
+# while ((count % 2) == 1):
+#     count = int(input("Enter number: "))
+count = 18
+temp = 0
+lock = count
+for x in range(1, (count*2), 2):
+    s = "#"
+    i = "1"
+    s = s * temp
+    i = i * (count - (lock - 1))
+    print(f"{i}{s}{i}")
+  
+    if temp < count2/2:
+        temp += 1
+        temp2 +=1
+    else:
+        temp2 -=1
+    
+    
+
+
+     
+    
+
+
