b5b1b2efce338ddff460bb3d47193b0f41739bad
souro268 <63824018+souro268@users.noreply.github.com>
2024-10-07 10:46:38 -0400

    Create U3-L5.py

---
+++ U3-L5.py
@@ -0,0 +1,54 @@
+#Modify 1
+def addFLO(a, b):
+    check1 = False
+    check2 = False
+    try:
+        a = float(a)
+        check1 = True
+    except:
+        print("Bad data")
+    try:
+        b = float(b)
+        check2 = True
+    except:
+        print("Bad data")
+    if (check1 == True and check2 == True):
+        return a+b
+    if not (check1 == True and check2 == True):
+        return "bad data"
+    
+    
+def addINT(a, b):
+    check1 = False
+    check2 = False
+    try:
+        a = int(a)
+        check1 = True
+    except:
+        print("Bad data")
+    try:
+        b = int(b)
+        check2 = True
+    except:
+        print("Bad data")
+    if (check1 == True and check2 == True):
+        return a+b
+    if not (check1 == True and check2 == True):
+        return "bad data"
+    
+    
+choice = input("Type 1 for Decimal numbers, and 2 for intergers: ")
+
+if (choice == '1'):
+    a = input("Enter the first value: ")
+    b = input("Enter the second value: ")
+    print(addFLO(a,b))
+elif (choice == '2'):
+    a = input("Enter the first value: ")
+    b = input("Enter the second value: ")
+    print(addINT(a,b))
+else:
+    print("Please enter 1 or 2")
+#Modify 2
+
+
