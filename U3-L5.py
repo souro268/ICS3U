41bad27e1223462fddbcfdd04839297cdc0c072b
souro268 <63824018+souro268@users.noreply.github.com>
2024-10-07 10:51:54 -0400

    Update U3-L5.py

--- U3-L5.py
+++ U3-L5.py
@@ -1,4 +1,4 @@
-#Modify 1
+import math as m
 def addFLO(a, b):
     check1 = False
     check2 = False
@@ -35,9 +35,26 @@ def addINT(a, b):
         return a+b
     if not (check1 == True and check2 == True):
         return "bad data"
-    
-    
-choice = input("Type 1 for Decimal numbers, and 2 for intergers: ")
+
+def myPOW(a,b):
+    check1 = False
+    check2 = False
+    try:
+        a = int(a)
+        check1 = True
+    except:
+        print("Bad data")
+    try:
+        b = int(b)
+        check2 = True
+    except:
+        print("Bad data")
+    if (check1 == True and check2 == True):
+        return m.pow(a,b)
+    if not (check1 == True and check2 == True):
+        return "bad data"
+
+choice = input("Type 1 for Decimal numbers \n 2 for intergers\n 3 for power: ")
 
 if (choice == '1'):
     a = input("Enter the first value: ")
@@ -47,8 +64,9 @@ elif (choice == '2'):
     a = input("Enter the first value: ")
     b = input("Enter the second value: ")
     print(addINT(a,b))
+elif (choice == '3'):
+    a = input("Enter the first value: ")
+    b = input("Enter the second value: ")
+    print(myPOW(a,b))
 else:
-    print("Please enter 1 or 2")
-#Modify 2
-
-
+    print("Please enter 1 or 2 or 3")
