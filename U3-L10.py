d5977c3093ec3c38a285a9293d4c9064817ddb77
souro268 <63824018+souro268@users.noreply.github.com>
2024-10-16 09:14:31 -0400

    Update U3-L10.py

--- U3-L10.py
+++ U3-L10.py
@@ -2,8 +2,8 @@ import math as m
 lists = []
 inputs = int(input("Please enter a integer thats >= 3: ")) # asks user for input
 
-if inputs >= 3: # checks if input is >= to 3
-    for n in range(3,inputs + 1, 1): 
+if inputs >= 3: # checks if input is >= to 3 
+    for n in range(3,inputs + 1, 1): # forloop that starts at 1
         if(n%2 == 1):  # checks if the number of odd
             a = m.pow(n,2) # math stuff
             b = m.pow((m.floor(n/2)) * (n+1),    2) # math stuff
@@ -12,7 +12,7 @@ if inputs >= 3: # checks if input is >= to 3
             if total == c: # checks if a + b(total) is equal to c, if so that means its a triple
                 lists.append(n) # adds n to the list of numbers so you know the starting
                 print(f"A = {m.sqrt(a)} \nB = {m.sqrt(b)} \nC = {m.sqrt(c)} \ntotal = {m.sqrt(total)}") # prints the values
-                print("----------------------------------------------")
+                print("----------------------------------------------") # prints
         elif(n%2 == 0): # checks if the number is even
             z = m.pow(n,2)/4 # math stuff
             a1 = m.pow(n,2) # math stuff
@@ -22,7 +22,7 @@ if inputs >= 3: # checks if input is >= to 3
             if total1 == c1: # checks if total (a+b) is equal to c
                 lists.append(n) # adds n to the list of numbers so you know the starting
                 print(f"A = {m.sqrt(a1)} \nB = {m.sqrt(b1)} \nC = {m.sqrt(c1)} \ntotal = {m.sqrt(total1)}") # prints the values
-                print("----------------------------------------------")
+                print("----------------------------------------------") # prints
     print(lists)
 else: # if the number is less than 3 then it tells the user to enter a value above 3
     print("Please enter a value above 3") # prints
