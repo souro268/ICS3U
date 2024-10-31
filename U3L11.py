1249547798b7739b5f811aa5e6bdd39a387816e8
souro268 <63824018+souro268@users.noreply.github.com>
2024-10-31 10:08:57 -0400

    Create U3L11.py

---
+++ U3L11.py
@@ -0,0 +1,28 @@
+import random as r
+def shuffle(A):
+    # Precondition: A must be a 1D array
+    #
+    # Randomizes a list
+    
+    # add your code here!
+    for x in range(0,len(A)-1):
+        index = r.randint(0,len(A)-1)
+        temp = A[x]
+        A[x] = A[index]
+        A[index] = A[x]
+    
+    return A
+    
+# driver code
+B= []
+inputs = int(input("Enter the size of your array: "))
+for x in range(0, inputs):
+    y = int(input(f"Enter number. You are at index {x+1}: "))
+    B.append(y)
+# compose the array with numbers from 1 to size, in order
+
+# add your code here!
+
+print(B) # before
+B = shuffle(B)
+print(B) # after
