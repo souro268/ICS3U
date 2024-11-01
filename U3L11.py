54bebb725043a98584b3308b1fbb33aed737fe53
souro268 <63824018+souro268@users.noreply.github.com>
2024-11-01 09:49:00 -0400

    Update U3L11.py

--- U3L11.py
+++ U3L11.py
@@ -9,16 +9,18 @@ def shuffle(A):
         index = r.randint(0,len(A)-1)
         temp = A[x]
         A[x] = A[index]
-        A[index] = A[x]
+        A[index] = temp
     
     return A
     
 # driver code
-B= []
+B= [1,2,3,4,5,6,7]
+
 inputs = int(input("Enter the size of your array: "))
 for x in range(0, inputs):
     y = int(input(f"Enter number. You are at index {x+1}: "))
     B.append(y)
+    
 # compose the array with numbers from 1 to size, in order
 
 # add your code here!
