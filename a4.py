3ee725f22c38e2bf637fc826771ba2306e3c1cf2
souro268 <63824018+souro268@users.noreply.github.com>
2024-12-16 09:32:31 -0500

    Update a4.py

--- a4.py
+++ a4.py
@@ -19,10 +19,28 @@ def mergeData(month, date, year, word):
     return year + month + date
     
 def bubblesort(Data_Arr)
+    num = [0]*1038
+    word = [0]*1038
     for i in range(len(Data_Arr)):
-        num, word = Data_Arr[i].strip()
+        num[i], word[i] = Data_Arr[i].strip()
+    bubble_sort_recur(num, i, j, n, word)
     
+    
+def bubble_sort_recur(num, i, j, n, word):
+    if j == n:
+        i = i+1
+        j = 0
+    if i == n:
+        return 
+    if num[i] > num[j]:
         
+        num[j], num[i] = num[i], num[j]
+        word[j], word[i] = word[i], word[j]
+        
+        bubble_sort_recur(a, i, j+1, n, word);
+    else:
+        bubble_sort_recur(a, i, j+1, n, word);
+    return num, word
     
     
 filename = 'wordle.dat'
@@ -38,3 +56,39 @@ for i in range(1038):
     
 
     
+
+
+
+---------------------TEST
+
+def bubble_sort_recur(num, i, j, n, word):
+    if j == n:
+        i = i+1
+        j = 0
+    if i == n:
+        return 
+    if num[i] > num[j]:
+        
+        num[j], num[i] = num[i], num[j]
+        word[j], word[i] = word[i], word[j]
+        
+        bubble_sort_recur(a, i, j+1, n, word);
+    else:
+        bubble_sort_recur(a, i, j+1, n, word);
+    return num, word
+
+# Oct 03 2023 WHILE
+# Oct 04 2023 SPURT
+# Oct 05 2023 BUNCH
+# Oct 06 2023 CHIME
+# 
+# 20231003 WHILE
+# 20231004 SPURT
+# 20231005 BUNCH
+# 20231006 CHIME
+num = [20231005, 20231006, 20231003, 20231004]
+word = ['BUNCH', 'CHIME', 'WHILE', 'SPURT']
+
+print(bubble_sort_recur(num, i, j, n, word))
+
+
