db9738bff294f77409c704bed6420617075c8208
souro268 <63824018+souro268@users.noreply.github.com>
2024-12-17 10:35:59 -0500

    Update a4.py

--- a4.py
+++ a4.py
@@ -6,8 +6,9 @@ Oct 03 2023 WHILE
 Oct 04 2023 SPURT
 Oct 05 2023 BUNCH
 Oct 06 2023 CHIME
+20231006-CHIME
 '''
-def mergeData(month, date, year, word):
+def mergeData(month, date, year):
     month = month.lower()
     montharr = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
     year_arr = []
@@ -18,100 +19,39 @@ def mergeData(month, date, year, word):
         month = '0' + month
     return year + month + date
     
-def bubblesort(Data_Arr)
-    num = [0]*1038
-    word = [0]*1038
-    for i in range(len(Data_Arr)):
-        num[i], word[i] = Data_Arr[i].strip()
-    bubble_sort_recur(num, i, j, n, word)
     
     
-def bubble_sort_recur(num, i, j, n, word):
-    if j == n:
-        i = i+1
-        j = 0
-    if i == n:
-        return 
-    if num[i] > num[j]:
-        
-        num[j], num[i] = num[i], num[j]
-        word[j], word[i] = word[i], word[j]
-        
-        bubble_sort_recur(a, i, j+1, n, word);
-    else:
-        bubble_sort_recur(a, i, j+1, n, word);
-    return num, word
+def bubble_sort_recursive(num, word_arr):
+    
+    for i in range(len(num)-1):
+        if num[i] > num[i + 1]:
+            temp = num[i]
+            num[i] = num[i+1]
+            num[i+1] = temp
+            
+            temp1 = word_arr[i]
+            word_arr[i] = word_arr[i+1]
+            word_arr[i+1] = temp1
+    return
     
     
 filename = 'wordle.dat'
 fh = open(filename, "r")  # Opens the file in read mode
-Data_Arr = [0]*1038
+num = ['']*1038
+word_arr = ['']*1038
 for i in range(1038):
     tempvar = fh.readline()
     tempvar.strip()
-    month, date, year, word =tempvar.split()
-    myData = mergeData(month, date, year, word)
-    Data_Arr[i] = myData + '-' + word
+    month, date, year, word = tempvar.split(' ')
+    myData = mergeData(month, date, year)
+    num[i] = str(myData)
+    word_arr[i] = str(word)
+    
+    bubble_sort_recursive(num, word_arr)
+#print(Data_Arr)
     
     
 
     
 
 
-
----------------------TEST
-
-def bubble_sort_recur(num, i, j, n, word):
-    if j == n:
-        i = i+1
-        j = 0
-    if i == n:
-        return 
-    if num[i] > num[j]:
-        
-        num[j], num[i] = num[i], num[j]
-        word[j], word[i] = word[i], word[j]
-        
-        bubble_sort_recur(a, i, j+1, n, word);
-    else:
-        bubble_sort_recur(a, i, j+1, n, word);
-    return num, word
-
-# Oct 03 2023 WHILE
-# Oct 04 2023 SPURT
-# Oct 05 2023 BUNCH
-# Oct 06 2023 CHIME
-# 
-# 20231003 WHILE
-# 20231004 SPURT
-# 20231005 BUNCH
-# 20231006 CHIME
-num = [20231005, 20231006, 20231003, 20231004]
-word = ['BUNCH', 'CHIME', 'WHILE', 'SPURT']
-
-print(bubble_sort_recur(num, i, j, n, word))
-
-
---------------- BUBBLE SORT --------------------------
-def bubble_sort_recursive(arr, n=None):
-    # Initialize n as the length of the array for the first call
-    if n is None:
-        n = len(arr)
-    
-    # Base case: If the array size is 1, it is already sorted
-    if n == 1:
-        return
-    
-    # Perform one pass of the bubble sort
-    for i in range(n - 1):
-        if arr[i] > arr[i + 1]:
-            # Swap if the element is greater than the next
-            arr[i], arr[i + 1] = arr[i + 1], arr[i]
-    
-    # Recursive call for the remaining array (excluding the last element)
-    bubble_sort_recursive(arr, n - 1)
-
-# Example usage
-numbers = [64, 34, 25, 12, 22, 11, 90]
-bubble_sort_recursive(numbers)
-print("Sorted array:", numbers)
