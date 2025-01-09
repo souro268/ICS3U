64e57d19c85ad114cdb053550634c1a7a447ed72
souro268 <63824018+souro268@users.noreply.github.com>
2025-01-09 10:50:22 -0500

    Update a5.py

--- a5.py
+++ a5.py
@@ -1,5 +1,6 @@
 def Date(Exp_Mo, Exp_Yr):
     Exp_Yr = str(Exp_Yr)
+    Exp_Mo = int(Exp_Mo)
     month = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
     for x in month:
         if Exp_Mo < 10:     
@@ -10,27 +11,19 @@ def Date(Exp_Mo, Exp_Yr):
         if Exp_Mo >= 10:
             Exp_Mo = str(Exp_Mo)
             return Exp_Yr + Exp_Mo
-    
-def mergeData(month, date, year):
-    # Converts month, day, and year into a single YYYYMMDD string.
-    month = month.lower()
-    month_arr = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
-    try:
-        for i in range(len(month_arr)):
-            if month == month_arr[i]:
-                month = str(i + 1)
-        if int(month) < 10:
-            # Padding single-digit months with a leading zero
-            month = '0' + month
-        return year + month + date
-    except:
-        print("Please enter a valid month and try again.")
-        User_Inputs(num_arr, word_arr, mergeData, merge_sort, binary_search, 0)
+merge_sort(DateArr, FirstNameArr, LastNameArr, CCtypeArr, CCNumber,  0, len(DateArr) - 1)
+def Comparison(DateArr, FirstNameArr, LastNameArr, CCtypeArr, CCNumber):
+    for x in range(len(DataArr)):
+        if DataArr[x] <= 202501:
+            print(f"{FirstNameArr[x]} {LastNameArr[x]}: ")
+            
+            Barbara Hadley:     Visa         #4532415491630710 202205 EXPIRED
+            
 
-
-
-def merge(arr, arr2, arr3, arr4, arr5, left, right):
+def merge(arr, arr2, arr3, arr4, arr5, left, mid, right):
     # Merges two sorted subarrays into a single sorted array.
+    for x in range(len(arr)):
+        arr[x] = int(arr[x])
     n1 = mid - left + 1
     n2 = right - mid
     L, L2, L3, L4, L5 = [0] * n1, [0] * n1, [0] * n1, [0] * n1, [0] * n1  # Temporary arrays for left subarray
@@ -48,25 +41,29 @@ def merge(arr, arr2, arr3, arr4, arr5, left, right):
     for j in range(n2):
         R[j] = arr[mid + 1 + j]
         R2[j] = arr2[mid + 1 + j]
+        R3[j] = arr3[mid + 1 + j]
+        R4[j] = arr4[mid + 1 + j]
+        R5[j] = arr5[mid + 1 + j]
     
     i = j = 0
     k = left
     while i < n1 and j < n2:
         if L[i] <= R[j]:
-            arr[k], arr2[k] = L[i], L2[i]
+            arr[k], arr2[k], arr3[k], arr4[k], arr5[k] = L[i], L2[i], L3[i], L4[i], L5[i]
+
             i += 1
         else:
-            arr[k], arr2[k] = R[j], R2[j]
+            arr[k], arr2[k], arr3[k], arr4[k], arr5[k] = R[j], R2[j], R3[j], R4[j], R5[j]
             j += 1
         k += 1
     
     while i < n1:
-        arr[k], arr2[k] = L[i], L2[i]
+        arr[k], arr2[k], arr3[k], arr4[k], arr5[k] = L[i], L2[i], L3[i], L4[i], L5[i]
         i += 1
         k += 1
     
     while j < n2:
-        arr[k], arr2[k] = R[j], R2[j]
+        arr[k], arr2[k], arr3[k], arr4[k], arr5[k] = R[j], R2[j], R3[j], R4[j], R5[j]
         j += 1
         k += 1
 
@@ -76,10 +73,13 @@ def merge_sort(arr, arr2, arr3, arr4, arr5, left, right):
     # Recursively sorts the array using the merge sort algorithm.
     if left < right:
         mid = (left + right) // 2
-        merge_sort(arr, arr2, arr3, arr4, arr5, left, right)
-        merge_sort(arr, arr2, arr3, arr4, arr5, left, right)
-        merge(arr, arr2, arr3, arr4, arr5, left, right)
-    #  
+        merge_sort(arr, arr2, arr3, arr4, arr5, left, mid)
+        merge_sort(arr, arr2, arr3, arr4, arr5, mid+1, right)
+        merge(arr, arr2, arr3, arr4, arr5, left, mid, right)
+        
+        
+       
+
     
 filename = 'data.dat'  # The name of the file containing Wordle data
 fh = open(filename, "r")  # Opens the file for
@@ -104,11 +104,12 @@ while n == 1:  # Loops through each line in the file
     tempvar.strip()  # Removes any leading/trailing whitespace
     try:
         First_Name, Last_Name, CC_Type, CC_Number, Exp_Mo, Exp_Yr = tempvar.split(',')
-        FirstNameArr.append(First_Name)
-        LastNameArr.append(Last_Name)
-        CCtypeArr.append(CC_Type)
-        CCNumber.append(CC_Number)
-        DateArr.append(Date(Exp_Mo, Exp_Yr))       
+        FirstNameArr.append(First_Name.strip())
+        LastNameArr.append(Last_Name.strip())
+        CCtypeArr.append(CC_Type.strip())
+        CCNumber.append(CC_Number.strip())
+        temp = Date(Exp_Mo, Exp_Yr.strip())
+        DateArr.append(temp)       
         
         
     except ValueError:
@@ -117,8 +118,8 @@ while n == 1:  # Loops through each line in the file
         print(f"There has been an error. Error message: {err=}. Error type: {type(err)=}")
         n = 0
 merge_sort(DateArr, FirstNameArr, LastNameArr, CCtypeArr, CCNumber,  0, len(DateArr) - 1)
+Comparison()
 
         
         
     
-
