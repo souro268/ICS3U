c98ed3e58e9c4f1f4e2dbbc5cca2e3fdb3cc355d
souro268 <63824018+souro268@users.noreply.github.com>
2025-01-14 09:24:44 -0500

    Create DMOJ.py

---
+++ DMOJ.py
@@ -0,0 +1,45 @@
+'''
+16 3 2 13
+5 10 11 8
+9 6 7 12
+4 15 14 1
+'''
+
+def Vertical():
+    temp = arr[0] + arr[4] + arr[8] + arr[12]
+    if temp == arr[3] + arr[7] + arr
+    
+    for x in range(4):
+        temp = arr[x] + arr2[x] + arr3[x] + arr4[x]
+        temp2 = arr[x+1] + arr2[x] + arr3[x] + arr4[x]
+def Hor():
+def Dia():
+def check():
+    if Vertical() == True and Hor() == True and Dia() == True:
+        return 'magic'
+    else:
+        return 'not magic'
+arr1 = []
+
+
+for x in range(4):
+    temp = input(" INPUT ")
+    one, two, three, four = temp.split(' ')
+    
+    one = int(one)
+    two = int(two)
+    three = int(three)
+    four = int(four)
+    
+    arr1.append(one)
+    arr1.append(two)
+    arr1.append(three)
+    arr1.append(four)
+
+arr = [arr1[0], arr1[1], arr1[2], arr1[3]]
+arr2 = [arr1[4], arr1[5], arr1[6], arr1[7]]
+arr3 = [arr1[8], arr1[9], arr1[10], arr1[11]]
+arr4 = [arr1[12], arr1[13], arr1[14], arr1[15]]
+
+
+
