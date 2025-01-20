129f1ea26e968892a029722bc952ba9cafc39ca6
souro268 <63824018+souro268@users.noreply.github.com>
2025-01-20 08:56:00 -0500

    Update DMOJ.py

--- DMOJ.py
+++ DMOJ.py
@@ -5,27 +5,29 @@ while n == 0:
     temp = input()
     if temp == 'quit!':
         n = 1
-    
     else:
         arr.append(temp)
-print(arr)
+        
 for x in range(len(arr)):
     try:
         if len(arr[x]) > 4:
             temp = arr[x]
             for y in range(len(temp)):
-                for i in range(len(v)):
-                    
-                    if temp[y] == 'o' and temp[y+1] == 'r':
-                        
-                        if temp[y-1] != v[i]:
+                if temp[y] == 'o' and temp[y+1] == 'r':
+                    for i in range(len(v)):
+                        if not temp[y-1] == v[i]:
                             temp[y] = 'ou'
+                            arr[x] = temp
                             
-                
+                            
         else:
             pass
     except:
         pass
+
+print(arr)
+        
+
 for x in range(len(arr)):
     print(arr[x])
     
