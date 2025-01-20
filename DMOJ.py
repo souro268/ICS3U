7c93d17f9a54982c1d953401faf5135918373bbf
souro268 <63824018+souro268@users.noreply.github.com>
2025-01-20 09:05:05 -0500

    Update DMOJ.py

--- DMOJ.py
+++ DMOJ.py
@@ -1,5 +1,12 @@
+def add(arr2):
+    temp = ''
+    for x in range(len(arr2)):
+        temp = temp + arr2[x]
+    return temp
+
 n = 0
 arr = []
+arr2 = []
 v = ['a', 'e', 'i', 'o', 'u', 'y']
 while n == 0:
     temp = input()
@@ -8,27 +15,26 @@ while n == 0:
     else:
         arr.append(temp)
         
+        
 for x in range(len(arr)):
-    try:
-        if len(arr[x]) > 4:
-            temp = arr[x]
-            for y in range(len(temp)):
-                if temp[y] == 'o' and temp[y+1] == 'r':
-                    for i in range(len(v)):
-                        if not temp[y-1] == v[i]:
-                            temp[y] = 'ou'
-                            arr[x] = temp
-                            
-                            
-        else:
-            pass
-    except:
+
+    if len(arr[x]) > 4:
+        temp = arr[x]
+        for i in range(len(temp)):
+            arr2.append(temp[i])
+        for y in range(len(temp)):
+            if arr2[y] == 'o' and arr2[y+1] == 'r':
+                for i in range(len(v)):
+                    if not arr2[y-1] == v[i]:
+                        arr.pop
+                        arr.pop
+                        arr2.append('our')
+                        arr[x] = add(arr2)
+                        
+                        
+    else:
         pass
 
-print(arr)
-        
 
-for x in range(len(arr)):
-    print(arr[x])
-    
+print(arr)
         
